6357cc1b2907eae8b57f9e936bb784fb84f7f42c
Nataless <151459521+Nataless@users.noreply.github.com>
2025-04-07 11:59:38 +0300

    Delete SQL  и получение данных directory

--- SQL  и получение данных/Домашнее задание по теме Работа с PostgreSQL (часть 1) Доп._задание.sql
+++
@@ -1,3 +0,0 @@
-select rating, row_number() over (order by title) as "Номер по порядку", title, min (title) over (partition by rating order by rating) as "Первое название фильма", max (title) over (partition by rating order by rating) as "Последнее название фильма"
-from film
-order by rating, title
